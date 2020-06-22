bd9fb57f7444e238b30008e5f47a59a8f04d820a
Nicole Zheng <zznicole@hotmail.com>
2020-06-22 12:37:58 +0200

    add clickBtn()

--- src/main.js
+++ src/main.js
@@ -1,12 +1,40 @@
-window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
 
-let myRecognition = new SpeechRecognition();
-myRecognition.interimResults = true;
-myRecognition.lang = 'en-US';
-myRecognition.maxAlternatives = 1;
-myRecognition.onaudiostart = function() {
-  console.log('Audio Capturing Started')
-}
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+// recognition.lang = 'en-US';
 
-console.log(myRecognition);
+
+function clickBtn() {
+
+  let p = document.createElement('p');
+  const words = document.querySelector('.words');
+  words.appendChild(p);
+  recognition.addEventListener('result', e => {
+    console.log(e.results);
+ 
+    const transcript = Array.from(e.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+      .join('')
+
+      p.textContent = transcript;
+      if(e.results[0].isFinal) {
+        p = document.createElement('p');
+        words.appendChild(p);
+      }
+
+    });
+
+  recognition.addEventListener('end', recognition.start);
+  recognition.start()
+};
+
+
+
+
+
+
+
+ 
